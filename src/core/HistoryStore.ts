f23524d946ad9f50b259cfbc0019f0bf7645d698
mikezzb <mikezhoudev@gmail.com>
2022-09-11 03:19:41 +0800

    Add video history store

---
+++ src/core/HistoryStore.ts
@@ -0,0 +1,37 @@
+import StoreManager from "./StoreManager";
+
+const LOAD_KEYS = ["videoHistory"];
+
+export default class HistoryStore extends StoreManager {
+  videoHistory: VideoHistoryItem[];
+  length: number;
+  constructor() {
+    super(LOAD_KEYS, LOAD_KEYS);
+    this.videoHistory ??= [];
+    this.length = this.videoHistory.length;
+  }
+  findItemIndex(url: string): number | null {
+    const result = this.videoHistory.findIndex((item) => item.url === url);
+    return result > -1 ? result : null;
+  }
+  addItem(url: string, item?: VideoHistoryItemBase): number {
+    let idx = this.findItemIndex(url);
+    item ??= { url };
+    if (idx === null) {
+      idx = this.initItem(url, item);
+    }
+    return idx;
+  }
+  initItem(url: string, item: VideoHistoryItemBase) {
+    this.videoHistory.push(item);
+    this.length += 1;
+    return this.length;
+  }
+  saveHistory() {
+    this.save("videoHistory", this.videoHistory);
+  }
+  updateItem(itemIndex: number, delta: Partial<VideoHistoryItem>) {
+    Object.assign(this.videoHistory[itemIndex], delta);
+    this.saveHistory();
+  }
+}
