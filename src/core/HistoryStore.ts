0eeff75223aa16e0263360cb9b9c8076211044e3
mikezzb <mikezhoudev@gmail.com>
2022-09-12 03:36:53 +0800

    Add check history for popup

--- src/core/HistoryStore.ts
+++ src/core/HistoryStore.ts
@@ -1,4 +1,4 @@
-import { makeObservable, observable } from "mobx";
+import { makeObservable, observable, action } from "mobx";
 import { MAX_RECORDS } from "../config";
 import StoreManager from "./StoreManager";
 
@@ -8,15 +8,25 @@ export default class HistoryStore extends StoreManager {
   videoHistory!: VideoHistoryItem[];
   length: number = 0;
   currIndex: number = -1;
+  prevItem?: VideoHistoryItem = undefined;
   constructor() {
     super(LOAD_KEYS, LOAD_KEYS);
     makeObservable(this, {
       length: observable,
       currIndex: observable,
+      prevItem: observable,
+      checkItem: action,
     });
   }
   get reversedHistory() {
-    return this.videoHistory.slice().reverse();
+    console.log("getter called");
+    console.log(this.prevItem);
+    const arr = [...this.videoHistory];
+    if (this.prevItem && this.currIndex > -1) {
+      console.log("removing prev item");
+      arr.splice(this.currIndex, 1);
+    }
+    return arr.reverse();
   }
   async init() {
     await this.loadStore();
@@ -38,17 +48,32 @@ export default class HistoryStore extends StoreManager {
     const result = this.videoHistory.findIndex((item) => item.url === url);
     return result > -1 ? result : null;
   }
+  /**
+   * For popup to see if curr url has prev hist, and mount the hist
+   * @param url
+   */
+  checkItem(url: string) {
+    let idx = this.findItemIndex(url);
+    if (idx !== null) {
+      this.prevItem = this.videoHistory[idx];
+      this.currIndex = idx;
+    }
+  }
   addItem(url: string, item?: VideoHistoryItemBase): number {
     let idx = this.findItemIndex(url);
     item ??= { url };
     if (idx === null) {
       idx = this.initItem(item);
+    } else {
+      // if has hist: make prev item for user to jump to prev playback time
+      this.prevItem = this.videoHistory[idx];
+      console.log("prev record: ", JSON.stringify(this.prevItem));
     }
     this.currIndex = idx;
     return idx;
   }
   initItem(item: VideoHistoryItemBase) {
-    this.videoHistory.push(item);
+    this.videoHistory.push(item as any);
     const index = this.shiftLength(1) - 1;
     return index;
   }
