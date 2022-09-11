eebf2c09e861d22cbfb94159dda5a180e2016129
mikezzb <mikezhoudev@gmail.com>
2022-09-12 01:35:42 +0800

    Add helper fns

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,3 +2,11 @@ export const getCurrUrl = async () => {
   const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
   return tabs[0].url as string;
 };
+
+export const prefix = (d: number) => (d > 9 ? `${d}` : `0${d}`);
+
+export const toMMSS = (seconds: number) => {
+  const min = prefix(Math.floor(seconds / 60));
+  const sec = prefix(+(seconds % 60).toFixed(0));
+  return `${min}:${sec}`;
+};
