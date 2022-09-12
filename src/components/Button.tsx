cd63eb11d5c183ad2bea078a7561e5f17eaa01ea
mikezzb <mikezhoudev@gmail.com>
2022-09-12 14:15:24 +0800

    style: update button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,11 +3,18 @@ import styled from "styled-components";
 const Button = styled.button`
   text-transform: capitalize;
   background-color: transparent;
-  border: 1px solid #0f0f0f;
-  color: #0f0f0f;
+  border: 1px solid #0f0f0f28;
+  color: #dd0055;
   border-radius: 4px;
-  padding: 4px 6px;
+  padding: 6px 12px;
   height: fit-content;
+  font-weight: 500;
+  font-size: 12px;
+  cursor: pointer;
+  :hover {
+    background-color: #dd005505;
+    border-color: #ac004236;
+  }
 `;
 
 export default Button;
