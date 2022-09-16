3d7cfb9e9ee506968dfa580d71edea68dda5bc19
mikezzb <mikezhoudev@gmail.com>
2022-09-16 23:19:15 +0800

    chore: webpack

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,14 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
-    mode: 'production'
+  mode: "production",
+  optimization: {
+    splitChunks: {
+      name: "vendor",
+    },
+    minimize: true,
+    minimizer: [new TerserPlugin({ parallel: true })],
+  },
 });
